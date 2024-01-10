e154cdf9ba7f948773a1d31485c2b6ac23f9281b
Vagner Nerves <vagnernervessantos@gmail.com>
2024-01-10 06:53:01 -0300

    feat: Ajusted profile for get user.

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
+
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+  const { user } = await getUserProfile.execute({
+    userID: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  Reflect.deleteProperty(user, 'password_hash')
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
